85f414f916c8400d83286e0c6d1cc835fc3b11db
laura <rosechebet1997@gmail.com>
2022-06-02 15:59:16 -0700

    python modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,14 +5,13 @@ def add_arg(argv):
     nlength = len(argv) - 1
     if nlength == 0:
         print("{:d}".format(nlength))
-        return
     else:
         i = 1
         add = 0
-        while i < nlength:
+        while i <= nlength:
             add += int(argv[i])
             i += 1
-            print("{:d}".format(add))
+        print("{:d}".format(add))
 
 
 if __name__ == "__main__":

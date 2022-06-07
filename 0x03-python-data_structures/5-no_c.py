894e0546fb4e96aedfe02717d51f5fd3a13c2acb
laura <rosechebet1997@gmail.com>
2022-06-07 14:53:44 -0700

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     separatestring = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
+        if i is not 'c' and i is not 'C':
             separatestring += i
     return(separatestring)

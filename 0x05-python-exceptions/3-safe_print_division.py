5c10ee579fa07a1e7b57d9e2b4f318b9cd075416
laura <rosechebet1997@gmail.com>
2022-06-20 03:57:51 -0700

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,7 @@ def safe_print_division(a, b):
         print("Inside result: {:.lf}".format(result))
     except ZeroDivisionError:
         result = None
+        print("Inside result: {:}".format(result))
     finally:
         #print("Inside result: {:.lf}".format(result))
         return result

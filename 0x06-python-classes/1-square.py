6723b9ab85a01140384d7c1966ecbc80a2391b18
laura <rosechebet1997@gmail.com>
2022-06-21 14:29:02 -0700

    exceptions

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+class square:
+    def__init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+class square:
+    def __init__(self, size=0):
+        self.size = size

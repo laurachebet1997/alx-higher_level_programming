932d52b4ffe681446b7ebde33bf493e74161af32
laura <rosechebet1997@gmail.com>
2022-06-21 13:30:40 -0700

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        #print("Inside result: {:.lf}".format(result))
     except Exception:
         result = None
-        #print("Inside result: {:}".format(result))
     finally:
         print("Inside result: {:.lf}".format(result))
     return result

46c1f4c56d0cfdef4fd5709f1f0b7963cb315dd3
laura <rosechebet1997@gmail.com>
2022-06-22 15:36:17 -0700

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""create class private attribute size"""
 
 
 class Square:
     """Class square with private instance attribute size"""
     def__init__(self, size):
-        """initializing class"""
         self.__size = size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,5 +2,30 @@
 
 
 class square:
+    """Initialize classSsquare"""
     def __init__(self, size=0):
-        self.size = size                                                              
+        """Init method of class Square"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            return (self.size * self.size)
+        def my_print(self):
+            if self.__size == 0:
+                print()
+                return
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                    print()

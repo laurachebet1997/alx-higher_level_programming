6070dfac12318e8fde662650621e7aecd62362e7
laura <rosechebet1997@gmail.com>
2022-07-01 13:58:04 -0700

    indentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,11 +12,9 @@ def add_integer(a, b):
         a = int(a)
     if type(b) == float:
         b = int(b)
-
-
     if not isinstance(a, int) or isinstance(a, bool):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) or isinstance(b, bool):
         raise TypeError("b must be an integer")
 
-    return ( a + b )
+    return (a + b)

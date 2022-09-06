31d915b2263ec5a161822d69c30434b3670f9a50
laura <rosechebet1997@gmail.com>
2022-09-06 13:35:03 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,7 @@ const { dict } = require('./101-data.js');
 const Dictn = {};
 for (const N in dict) {
   if (Dictn[dict[N]] === undefined) {
-  Dictn[dict[N]] = [];
+    Dictn[dict[N]] = [];
   }
   Dictn[dict[N]].push(N);
 }

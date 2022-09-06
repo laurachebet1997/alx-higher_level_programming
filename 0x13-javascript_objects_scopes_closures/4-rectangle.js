892668ab41d1326b80fdaa0f5652d384ed1fcc43
laura <rosechebet1997@gmail.com>
2022-09-06 14:42:27 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+
+module.exports = Rectangle;
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -29,4 +31,4 @@ class Rectangle {
   }
 }
 
-module.exports = Rectangle;
+//module.exports = Rectangle;

0bfd380ef665b9932047fd65edb6952a58a36646
laura <rosechebet1997@gmail.com>
2022-09-13 06:35:15 -0700

    fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,16 +2,14 @@
 '''
 create a model for city using base
 '''
-
+import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeingnKey
 from model_state import Base, State
 
 
 class City(Base):
-    '''
-    Representing a class city
-    '''
-    __tablename__ = 'cities'
+    '''Representing a class city'''
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))

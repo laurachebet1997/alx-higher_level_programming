88818a8e9db986163212fd51606b655c4ca6c02c
laura <rosechebet1997@gmail.com>
2022-09-13 06:08:51 -0700

    fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,13 +10,12 @@ from sys import argv
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                      argv[2],
+                                                                      argv[3]))
+    Base.metadata.create_all(engin)
+    Session = sessionmaker(bind=engin)
     session = Session()
     rows = session.query(City, State).filter(City.state_id == State.id)\
                                      .order_by(City.id).all()

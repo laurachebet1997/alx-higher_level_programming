896b7fd146e62f3c693a85c473916af7bfa3b6d4
laura <rosechebet1997@gmail.com>
2022-09-13 05:07:10 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
+'''
+a script that lists all states                                                                                                                       from the database                                                                                                                                    
+'''
 
 import MySQLdb
 from sys import argv
 
-'''
-a script that lists all states
-from the database
-'''
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                           password=argv[2], database=argv[3])

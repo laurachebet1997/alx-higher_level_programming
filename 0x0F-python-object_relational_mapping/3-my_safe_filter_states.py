6a317a944df9631dac8513466a87aa46ff35eeca
laura <rosechebet1997@gmail.com>
2022-09-13 05:12:55 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
+'''
+script that lists all states from the database
+'''
 
 import MySQLdb
 from sys import argv
 
-'''
-script that lists all states from the database
-'''
 if __name__ == "__main__":
-    cont = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
-    cursor = cont.cursor()
+    cursor = db.cursor()
     cursor.execute(
         "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-        (argv[4],)
-        )
+        (argv[4],))
     db = cursor.fetchall()
     for i in db:
         print(i)
+    cursor.close()
+    db.close()

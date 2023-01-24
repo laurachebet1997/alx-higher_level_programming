b27899c02dc76d2a05129377a6562bb92e283aa3
laurachebet1997 <rosechebet19971997@gmail.com>
2023-01-24 23:21:10 +0300

    correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, TypeError, ValueError):
         result = None
     finally:
-        if result != None:
-            print("Inside result: {:.1f}".format(result))
-        else:
-            return result
+        print("Inside result: {:.1f}".format(result))
+        return result

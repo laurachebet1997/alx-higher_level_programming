9395c8a21c6d13b3c7405f9b86761a5e2b41e999
laurachebet1997 <rosechebet19971997@gmail.com>
2023-01-31 15:54:53 +0300

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,64 +1,76 @@
 #!/usr/bin/python3
-"""Define Rectangle Class
+"""Contains classes for working with Polygons.
 """
-
-from models.rectangle import Rectangle
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Module Representation of Square
-"""
+    """Represents a polygon with 4 perpendicular and
+    equal sides.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization a Square
+        """Initializes a new square object.
+        Args:
+            size (int): The width and height of this square.
+            x (int): The horizontal position of this square.
+            y (int): The vertical position of this square.
+            id (int): The id of this square.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """module Square size getter
+        """Gets or sets the size of this square.
         """
         return self.width
 
     @size.setter
     def size(self, value):
-        """module Square size setter
+        """Gets or sets the size of this square.
         """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """module string represation of square
+        """Creates a string representation of this polygon.
+        Returns:
+            str: A string representation of this polygon.
         """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                         self.x,
-                                                         self.y,
-                                                         self.width)
+        parts = (
+            self.id,
+            self.x,
+            self.y,
+            self.width
+        )
+        res = '[Square] ({}) {:d}/{:d} - {:d}'.format(
+            parts[0], parts[1], parts[2], parts[3]
+        )
+        return res
 
     def update(self, *args, **kwargs):
-        """module update square
+        """Updates the attributes of this polygon.
+        Args:
+            args (tuple): A tuple of non-keyword arguments.
+            kwargs (dict): A dictionary of keyword arguments.
         """
-        if len(args):
-            for i, arg in enumerate(args):
-                if i == 0:
-                    self.id = arg
-                elif i == 1:
-                    self.size = arg
-                elif i == 2:
-                    self.x = arg
-                elif i == 3:
-                    self.y = arg
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key) is True:
-                    setattr(self, key, value)
+        attrs = ('id', 'size', 'x', 'y')
+        for key, val in zip(attrs, args):
+            setattr(self, key, val)
+        if (type(args) is None or len(args) == 0) and (type(kwargs) is dict):
+            for key, val in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, val)
 
     def to_dictionary(self):
-        """retrun dictonary
+        """Creates a dictionary representation of this polygon.
+        Returns:
+            dict: A dictionary representation of this polygon.
         """
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
+        res = {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
         }
+        return res

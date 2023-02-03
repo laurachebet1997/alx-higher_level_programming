c37f7c1cc8bd43f46707f258f293388d9b2adbdc
laurachebet1997 <rosechebet19971997@gmail.com>
2023-02-03 12:05:36 +0300

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,7 @@ Python script takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == '__main__':
